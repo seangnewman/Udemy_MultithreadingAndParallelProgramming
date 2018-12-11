e999bca274196c16116684a66e5379982b91f5c3
newmanuevers <seangnewman@msn.com>
2018-12-10 20:42:38 -0700

    Thread API, Thread Pool, APM & EAP

---
+++ Udemy_MultithreadingAndParallelProgramming/PrintingInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_MultithreadingAndParallelProgramming
+{
+    public class PrintingInfo
+    {
+        public int ProcessedNumbers { get; set; }
+
+    }
+}
